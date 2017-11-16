2fc2e3ef09ebb98563c0d49fc5ad1574b1937ba5
DOUELLE, Philippe <philippe.douelle@epitech.eu>
2017-11-16 10:07:07 +0100

    new project

---
+++ Dichotomie/DichotomieWeb/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Dichotomie.Models;
+
+namespace DichotomieWeb.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual List<Topic> Topics { get; set; }
+        public virtual List<Reply> Replies { get; set; }
+    }
+}
