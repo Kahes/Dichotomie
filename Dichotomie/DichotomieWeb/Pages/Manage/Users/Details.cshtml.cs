be37df818591e0499ebd1eac2ac5c1d9c2267ea7
pdouelle <douelle.p@gmail.com>
2017-11-21 23:39:02 +0100

    I created the users details page

---
+++ Dichotomie/DichotomieWeb/Pages/Manage/Users/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DichotomieWeb.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DichotomieWeb.Pages.Manage.Users
+{
+    [Authorize(Roles = "Admin, Moderator")]
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DetailsModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser = await _userManager.FindByIdAsync(id);
+
+            if (ApplicationUser == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
