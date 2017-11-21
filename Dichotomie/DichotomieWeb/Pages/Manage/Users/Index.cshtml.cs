2e034e3bb45587c54468c72567b77ea0d4dbcfce
pdouelle <douelle.p@gmail.com>
2017-11-21 23:03:56 +0100

    I created manage user page where users are listed. Only admins or moderators can access this page.

---
+++ Dichotomie/DichotomieWeb/Pages/Manage/Users/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DichotomieWeb.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DichotomieWeb.Pages.Manage.Users
+{
+    [Authorize(Roles = "Admin, Moderator")]
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ApplicationUser> ApplicationUser { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            ApplicationUser = await _context.Users.ToListAsync();
+        }
+    }
+}
